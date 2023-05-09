a639840b15496d8e20f6356558aab3a11b1d3bcd
Fahmi Ramdhani <84432472+ramdhanifahmi@users.noreply.github.com>
2023-05-09 22:39:51 +0700

    Create project structure
    - initial commit

---
+++ spring-boot-crud-project/src/main/java/com/dev/fahmi/FahmiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.fahmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahmiApplication.class, args);
+	}
+
+}
